4ee691303a975d93a662232512fc8b246ab8f4e6
zoom26042604 <nathan.ferre@ynov.com>
2026-05-07 09:01:41 +0200

    init: setup Maven and module-info base for Wishtris Tetris project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module wishtris {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports com.wishtris;
+    exports com.wishtris.game;
+    exports com.wishtris.model;
+    exports com.wishtris.ui;
+}
